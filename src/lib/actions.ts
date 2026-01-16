627b1f8632d76d557daf54f606694d34f4c5c969
LinkolnK <linkolnkrauskopf@gmail.com>
2026-01-15 21:26:12 -0700

    fixed and updated the connection issues to the database

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,63 +1,63 @@
-'use server'
+// 'use server'
 
-import { prisma } from '@/lib/prisma'
-import { headers } from 'next/headers'
-import { auth } from './auth'
+// import { prisma } from '@/lib/prisma'
+// import { headers } from 'next/headers'
+// import { auth } from './auth'
 
-export async function createClient(formData: FormData) {
+// export async function createClient(formData: FormData) {
 
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
+//   const session = await auth.api.getSession({
+//     headers: await headers()
+//   })
 
-  if (!session) {
-    throw new Error('Not logged in')
-  }
+//   if (!session) {
+//     throw new Error('Not logged in')
+//   }
 
-  const userId = session.user.id
+//   const userId = session.user.id
 
-  const name = formData.get('name') as string
-  const email = formData.get('email') as string
+//   const name = formData.get('name') as string
+//   const email = formData.get('email') as string
   
-    const clients = await prisma.client.create({
-      data: {
-        name,
-        email,
-        userId,
-      }
-    })
+//     const clients = await prisma.client.create({
+//       data: {
+//         name,
+//         email,
+//         userId,
+//       }
+//     })
 
-}
+// }
 
-export async function createInvoice(formData: FormData) {
+// export async function createInvoice(formData: FormData) {
 
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
+//   const session = await auth.api.getSession({
+//     headers: await headers()
+//   })
 
-  if (!session) {
-    throw new Error('Not logged in')
-  }
+//   if (!session) {
+//     throw new Error('Not logged in')
+//   }
 
-  const userId = session.user.id
+//   const userId = session.user.id
 
-  const amount = parseFloat(formData.get('amount') as string)
-  const dueDate = formData.get('dueDate') as string
-  const isPaid = formData.get('isPaid') === 'true'
-  const clientId = formData.get('clientId') as string
+//   const amount = parseFloat(formData.get('amount') as string)
+//   const dueDate = formData.get('dueDate') as string
+//   const isPaid = formData.get('isPaid') === 'true'
+//   const clientId = formData.get('clientId') as string
 
  
-    const invoices = await prisma.invoice.create({
-      data: {
-        amount,
-        dueDate: new Date(dueDate),
-        isPaid,
-        clientId,
-        userId,
-      }
-    })
+//     const invoices = await prisma.invoice.create({
+//       data: {
+//         amount,
+//         dueDate: new Date(dueDate),
+//         isPaid,
+//         clientId,
+//         userId,
+//       }
+//     })
 
-}
+// }
 
 
 
